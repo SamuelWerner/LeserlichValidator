1339270ed68926ecfa4f06af67b91dac9c34c7b9
Margaret Woltschanski <margaret.woltschanski@iem.thm.de>
2018-11-11 17:45:30 +0100

    hintergrund erste Versuche Part2

--- validate/Kontraste/hintergrund.js
+++ validate/Kontraste/hintergrund.js
@@ -1,9 +1,17 @@
 function background (dom){
+    result = "";
     var i = 0;
     for (let node of dom.window.document.querySelectorAll('*')){ //querySelectorAll to get Elements from css code
-        if (node.color){
-            console.log(node);
+        if (!node.color) continue
+        if (!node.backgroundColor) continue
+
+        for (let pseudo of ['',':before',':after']){
+            node.classList.add("validationMarker"+i);
+            result += "Zeile " + lineOfCode.lineOfCode(dom.window.document.documentElement.innerHTML, "validationMarker"+i) + ": " +"</br>";
+            i++;
+            break;
         }
 
     }
+    return result;
 }

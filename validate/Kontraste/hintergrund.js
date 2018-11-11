bbd86e38dc0014ec04568c47997302a6723e34d3
Margaret Woltschanski <margaret.woltschanski@iem.thm.de>
2018-11-11 21:12:32 +0100

    hintergrund teste ausgabe

--- validate/Kontraste/hintergrund.js
+++ validate/Kontraste/hintergrund.js
@@ -1,17 +1,16 @@
-function background (dom){
-    result = "";
-    var i = 0;
-    for (let node of dom.window.document.querySelectorAll('*')){ //querySelectorAll to get Elements from css code
-        if (!node.color) continue
-        if (!node.backgroundColor) continue
+var lineOfCode = require('../../../lineOfCode');
 
-        for (let pseudo of ['',':before',':after']){
-            node.classList.add("validationMarker"+i);
-            result += "Zeile " + lineOfCode.lineOfCode(dom.window.document.documentElement.innerHTML, "validationMarker"+i) + ": " +"</br>";
-            i++;
-            break;
-        }
-
+exports.validate = function(dom) {
+    var result = background(dom);
+    if (result == ""){
+        return "<div class='alert alert-success'>Validation erfolgreich.</div>"
+    } else {
+        return "<div class='alert alert-warning'>Validation fehlgeschlagen</b>: </br>" + result + "</div>";
     }
-    return result;
+
+}
+function background (dom){
+   result = "";
+
+   return result;
 }

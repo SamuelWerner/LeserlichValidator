a4a8b7aced3287dffb8f41a300e6b67a6faa514b
Samuel Werner <hallo@samuelwerner.de>
2018-11-18 22:15:05 +0100

    Der Validation weitere Ausnahmen hinzugefügt. Code optimiert.

--- validate/Schrift/Zeichen/Schriftart/humanistischeGroteskschriften.js
+++ validate/Schrift/Zeichen/Schriftart/humanistischeGroteskschriften.js
@@ -5,33 +5,27 @@ exports.validate = function(dom) {
     if (result == ""){
         return "<div class='alert alert-success'>Validation erfolgreich.</div>"
     } else {
-        return "<div class='alert alert-warning'>Validation fehlgeschlagen</b>: </br>" + result + "</div>";
+        return "<div class='alert alert-warning'>Es wurden abweichende Schriften erkannt. Evtl prüfen:</b>: </br>" + result + "</div>";
     }
-
 }
 
 function fonts (dom) {
     result = "";
-    let fonts = []
     var i = 0;
     for (let node of dom.window.document.querySelectorAll('*')) {
         if (!node.textContent) continue
         if (!node.style) continue
 
         for (let pseudo of ['', ':before', ':after']) {
-
             let fontFamily = dom.window.getComputedStyle(node, pseudo).fontFamily +"";
-
-
             if (fontFamily && fontFamily !== ""){
-                if (!fontFamily.includes("sans-serif") && !fontFamily.includes("inherit")){
+                if (!fontFamily.includes("sans-serif") && !fontFamily.includes("inherit") && !fontFamily.includes("-webkit-small-control") && !fontFamily.includes("monospace")){
                     node.classList.add("validationMarker"+i);
                     result += "Zeile " + lineOfCode.lineOfCode(dom.window.document.documentElement.innerHTML, "validationMarker"+i) + ": " +fontFamily+"</br>";
                     i++;
                     break;
                 }
             }
-
         }
     }
     return result;

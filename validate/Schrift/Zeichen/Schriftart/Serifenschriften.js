03c62b58b5bf4537d71b2ee3b5c6a2b990319f93
Samuel Werner <hallo@samuelwerner.de>
2018-11-20 00:11:24 +0100

    Lint Warnungen behoben

--- validate/Schrift/Zeichen/Schriftart/Serifenschriften.js
+++ validate/Schrift/Zeichen/Schriftart/Serifenschriften.js
@@ -2,19 +2,19 @@ var lineOfCode = require('../../../lineOfCode');
 
 exports.validate = function(dom) {
     var result = fonts(dom);
-    if (result == ""){
+    if (result === ""){
         return "<div class='alert alert-success'>Validation erfolgreich.</div>"
     } else {
-        return "<div class='alert alert-warning'>Es wurden Schriften mit Serifen erkannt. Falls die Serifen beibehalten werden sollen, prüfen ob durch Renaissance-Antiqua ersetzen:</b>: </br>" + result + "</div>";
+        return "<div class='alert alert-warning'>Es wurden Schriften mit Serifen erkannt. Falls die Serifen beibehalten werden sollen, kann als Alternative Renaissance-Antiqua verwendet werden:</b>: </br>" + result + "</div>";
     }
-}
+};
 
 function fonts (dom) {
     result = "";
     var i = 0;
     for (let node of dom.window.document.querySelectorAll('*')) {
-        if (!node.textContent) continue
-        if (!node.style) continue
+        if (!node.textContent) continue;
+        if (!node.style) continue;
 
         for (let pseudo of ['', ':before', ':after']) {
             let fontFamily = dom.window.getComputedStyle(node, pseudo).fontFamily +"";

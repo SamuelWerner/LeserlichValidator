a485423dbcb952a702eb7a0988d6f62af13a1671
Samuel Werner <hallo@samuelwerner.de>
2018-12-11 14:19:44 +0100

    Protokol dynamisch einsetzen

--- routes/validateWebsite.js
+++ routes/validateWebsite.js
@@ -1,25 +1,21 @@
 const express = require('express');
 const session = require('express-session');
-const fs = require("fs");
 const cheerio = require('cheerio');
 const phantom = require('phantom');
 const router = express.Router();
-/* GET home page. */
+
 router.get('/', function(req, res) {
 
     (async function() {
         session.validationReady = false
         const instance = await phantom.create();
         const page = await instance.createPage();
-        await page.on('onResourceRequested', function(requestData) {
-            //console.info('Requesting', requestData.url);
-        });
-
-        const status = await page.open(session.renderURL);
+        await page.open(session.renderURL);
         const content = await page.property('content');
         await instance.exit();
 
         host = req.headers.host
+        protocol = req.protocol
         var cheer = cheerio.load(content);
         cheer('link').each(function(i, elem) {
             let href = cheer(this).attr('href')
@@ -42,7 +38,7 @@ router.get('/', function(req, res) {
             console.log(src)
         });
 
-        cheer('<script src="http://'+host+'/js/validate.js"></script>').insertAfter('body');
+        cheer('<script src="'+protocol+'://'+host+'/js/validate.js"></script>').insertAfter('body');
         cheer('<script src="https://code.jquery.com/jquery-3.3.1.min.js"></script>').insertAfter('body');
         res.send(cheer.html());
     })();
